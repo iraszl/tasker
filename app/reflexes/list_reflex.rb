b62363e0eb6ec41e3ca9aacdd70198c06d8d4331
Ivan Raszl <iraszl@gmail.com>
2022-03-27 15:38:11 +0800

    alternative validation

--- app/reflexes/list_reflex.rb
+++ app/reflexes/list_reflex.rb
@@ -1,12 +1,7 @@
 class ListReflex < StimulusReflex::Reflex
   def create_task
-    sleep 2
     list = List.find(element.dataset.list_id)
-    task = list.tasks.create(task_params)
-    if task.errors.present?
-      error_messages = task.errors.full_messages.join", "
-      raise error_messages
-    end
+    @new_task = list.tasks.create(task_params)
   end
 
   private
